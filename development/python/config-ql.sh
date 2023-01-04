23fefea15ba7003fc005255ee2c2d48dd5402c86
brandimarte <brandimarte@pm.me>
2023-01-04 23:07:04 +0100

    feat: python env with quantum computing libraries

---
+++ development/python/config-ql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+python_version=3.10.6
+base_path=${HOME}/local/opt/python
+install_path=${base_path}/miniforge3/${python_version}-ql
+
+wget https://github.com/conda-forge/miniforge/releases/download/22.9.0-1/Miniforge3-22.9.0-1-Linux-x86_64.sh
+
+bash Miniforge3-22.9.0-1-Linux-x86_64.sh -b -p "${install_path}"
+
+module load python
+
+conda install -k -y python=${python_version}
+conda install pylint pycodestyle autopep8 cpplint cppcheck shellcheck jupyter isort pytest pennylane matplotlib pillow pylatexenc seaborn pyparsing contourpy kiwisolver fonttools pandas pytz pydot qiskit-terra qiskit-ibmq-provider
+pip install compdb
+pip install qiskit[visualization]
+
+conda clean -y -a
+
+exit
---
+++ modules/python/mforge-3.10.6-ql.lua
@@ -0,0 +1,31 @@
+local modulename = "python"
+local distrib = "miniforge3"
+local version = "3.10.6-ql"
+local basepath = pathJoin(os.getenv("HOME"), "local/opt", modulename, distrib, version)
+
+whatis("Loads "..modulename.." ("..version..") from "..distrib.." distribution")
+
+-- conflicts
+conflict("python")
+
+setenv('CONDA_PREFIX', basepath)
+setenv('CONDA_EXE', pathJoin(basepath, "bin/conda"))
+setenv('CONDA_PYTHON_EXE', pathJoin(basepath, "/bin/python"))
+setenv('CONDA_DEFAULT_ENV', "base")
+setenv('CONDA_SHLVL', '1')
+-- setenv('_CE_M', '')
+-- setenv('_CE_CONDA', '')
+
+prepend_path('PATH', pathJoin(basepath, "/condabin"))
+prepend_path('PATH', pathJoin(basepath, "/bin"))
+prepend_path('PYTHONPATH', pathJoin(basepath, "/bin"))
+prepend_path('INCLUDE', pathJoin(basepath, "/include"))
+prepend_path('CPATH', pathJoin(basepath, "/include"))
+-- -- prepend_path('LD_LIBRARY_PATH', pathJoin(basepath, "/lib"))
+prepend_path('LIBRARY_PATH', pathJoin(basepath, "/lib"))
+prepend_path('PKG_CONFIG_PATH', pathJoin(basepath, "/lib/pkgconfig"))
+prepend_path('MANPATH', pathJoin(basepath, "/share/man"))
+
+-- To activate conda's base environment in your current shell session:
+-- eval "$(${HOME}/local/opt/python/miniforge3/3.10.8/bin/conda shell.bash hook)"
+
