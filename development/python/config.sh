6cd90f05a33b81a1b6719b66232c4dafc1332e5e
brandimarte <brandimarte@pm.me>
2023-04-13 11:37:56 +0200

    mnt: update python package list

--- development/python/config.sh
+++ development/python/config.sh
@@ -12,9 +12,11 @@ bash Miniforge3-22.9.0-1-Linux-x86_64.sh -b -p "${install_path}"
 
 module load python
 
+conda config --set channel_priority flexible
+
 conda install -k -y python=${python_version}
 conda install autopep8 conda=22.9.0 cppcheck cpplint isort jupyter libjpeg-turbo=2.1.4 libpng matplotlib numpy opencv pillow pycodestyle pylint pytest pytorch shellcheck torchvision visdom
-pip install compdb thop
+pip install compdb thop vcd
 conda clean -y -a
 
 exit
